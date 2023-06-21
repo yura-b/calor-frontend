9a25561db08d8d4f5f462a4a306de690f0c74505
Yurii Koshyk <y.koshyk12@gmail.com>
2023-06-21 14:17:29 +0300

    adding empty folders / format files

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
-import Head from '@layouts/user/Head'
+import Head from '@/layouts/Head';
 
 interface Props {
   props: any;
 }
 
 const HomePage: React.FC<Props> = (): React.ReactElement => {
-  return (
-    <Head title="Home Page" />
-  )
+  return <Head title="Home Page" />;
 };
 
 export default HomePage;
