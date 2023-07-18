8e75ea240c2ca2f28045418ea5924e38586c1e3f
smolynetso <oksana.smolynets@bart-solutions.com>
2023-07-18 11:19:28 +0300

    added items to main menu

--- src/components/MainLayout/components/Header/Header.tsx
+++ src/components/MainLayout/components/Header/Header.tsx
@@ -20,23 +20,25 @@ const Header: React.FC = (): React.ReactElement => {
     setIsCartOpen(false);
   };
   return (
-    <div className="flex flex-col w-full h-auto bg-custom-red items-center justify-around lg:flex-row lg:h-20 px-6 py-3">
-      <div className="flex flex-row items-center justify-between w-full">
-        <div className="flex w-7 lg:w-44">
-          <img src={logoImg} alt="" className="w-7 h-7 sm:w-7 sm:h-7 lg:w-11 lg:h-11" />
-          <img src={logoText} alt="" className="hidden lg:block" />
+    <div className=" w-full h-auto bg-custom-red px-6 py-3">
+      <div className=" flex flex-col xl:max-w-[86vw] m-auto items-center justify-center lg:flex-row">
+        <div className="flex flex-row items-center justify-center w-full">
+          <div className="flex w-7 lg:w-44">
+            <img src={logoImg} alt="" className="w-7 h-7 sm:w-7 sm:h-7 lg:w-11 lg:h-11" />
+            <img src={logoText} alt="" className="hidden lg:block" />
+          </div>
+          <div className="hidden lg:block">
+            <MainMenu />
+          </div>
+          <Busket count={2} onClick={openCart} />
+          <div>
+            <img src={burgerIcon} alt="" className="block lg:hidden" />
+          </div>
         </div>
-        <div className="hidden lg:block">
-          <MainMenu />
-        </div>
-        <Busket count={2} onClick={openCart} />
-        <div>
-          <img src={burgerIcon} alt="" className="block lg:hidden" />
-        </div>
-      </div>
-      <div className="w-full flex flex-row justify-center mt-3 lg:mt-0">
-        <div className="w-full">
-          <SearchInput />
+        <div className="w-full flex flex-row justify-center mt-3 lg:mt-0 lg:basis-1/5">
+          <div className="w-full">
+            <SearchInput />
+          </div>
         </div>
       </div>
       <Modal className="flex items-center justify-center" open={isCartOpen} onClose={closeCart}>
