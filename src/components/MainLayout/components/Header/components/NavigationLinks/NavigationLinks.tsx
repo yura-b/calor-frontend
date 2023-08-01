b7141e8a6d193bb03520adf39a3396383cb8929c
smolynetso <oksana.smolynets@bart-solutions.com>
2023-08-01 19:15:08 +0300

    fixed navigation links

--- src/components/MainLayout/components/Header/components/NavigationLinks/NavigationLinks.tsx
+++ src/components/MainLayout/components/Header/components/NavigationLinks/NavigationLinks.tsx
@@ -4,14 +4,14 @@ import { paths } from '@routes/paths';
 import styles from '@/styles/Styles.module.scss';
 
 const NavigationLinks: React.FC = (): React.ReactElement => {
-  const convertToTitleCase = (str: string) =>
-    str
-      .replace(/_./g, (match) => match[1].toUpperCase())
-      .replace(/^(.)/, (match) => match.toUpperCase())
-      .replace(/([A-Z])/g, ' $1')
+  const convertToTitleCase = (str: string) => {
+    return str
+      .replace(/_/g, ' ')
+      .replace(/(^\w{1}|\s+\w{1})/g, (match) => match.toUpperCase())
       .trim();
-  const currentPage = convertToTitleCase(window.location.href.split('/')[3]);
+  };
 
+  const currentPage = convertToTitleCase(window.location.pathname.slice(1));
   return (
     <div className={`${styles.body1} flex text-white justify-center mt-4 lg:hidden`}>
       {window.location.href.split('/')[3] != '' && (
--- src/routes/paths.ts
+++ src/routes/paths.ts
@@ -1,7 +1,7 @@
 export const paths = {
   home: '/',
-  design_shoe: '/design_shoe',
-  design_bag: '/design_bag',
+  design_shoe: '/design_your_shoe',
+  design_bag: '/design_your_bag',
   accessories: '/accessories',
   shoe_care_product: '/shoe_care_product',
   customer_experience: '/customer_experience',
