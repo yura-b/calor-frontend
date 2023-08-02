7a8a6f9beb1a964dde150e9a8720c4b0d33f8503
Yurii Koshyk <y.koshyk12@gmail.com>
2023-08-02 19:01:55 +0300

    adding aditional data to form data

--- src/api/pages.ts
+++ src/api/pages.ts
@@ -8,17 +8,19 @@ export const getPageSection = () => {
   return axios.get(`${backendUrl}/manager/pages`)
 }
 
-export const saveChanges = (access_token: string, data: PageSection[]) =>{
-    return axios.put(`${backendUrl}/manager/pages`, data, authorization(access_token))
+export const saveChanges = (access_token: string, data: PageSection[]) => {
+  return axios.put(`${backendUrl}/manager/pages`, data, authorization(access_token))
 }
 
-export const uploadEventPhoto = (file) =>{
+export const uploadEventPhoto = (file) => {
   const formData = new FormData();
-  
+
   formData.append('file', file);
-    return axios.post(`${backendUrl}/do/upload`, formData, {
-      headers: {
-        'Content-Type': 'multipart/form-data'
-      },
-    })
+  formData.append('folder', 'events');
+
+  return axios.post(`${backendUrl}/do/upload`, formData, {
+    headers: {
+      'Content-Type': 'multipart/form-data'
+    },
+  })
 }
