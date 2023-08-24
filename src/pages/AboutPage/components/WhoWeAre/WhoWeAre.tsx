65e8401002e5e4f06ca5d8a61a90fb5739d5d874
smolynetso <oksana.smolynets@bart-solutions.com>
2023-08-24 13:19:15 +0300

    about read more changes

--- src/pages/AboutPage/components/WhoWeAre/WhoWeAre.tsx
+++ src/pages/AboutPage/components/WhoWeAre/WhoWeAre.tsx
@@ -1,6 +1,7 @@
 import styles from '@styles/Styles.module.scss';
 import { motion } from 'framer-motion';
 import { fadeAnimation } from '@styles/Animations';
+import ReadMore from '@/components/ReadMore';
 interface Props {
   whoWeAre?: {
     title: string;
@@ -8,12 +9,18 @@ interface Props {
   };
 }
 const WhoWeAre: React.FC<Props> = ({ whoWeAre }): React.ReactElement => {
+  const initialText = whoWeAre?.value.match(/[^.!]+[.!]/g)?.slice(0, 3) || '';
+  const expandedText = whoWeAre?.value || '';
   <h1 className={`${styles.header1} text-white text-center`}>?</h1>;
   return (
-    <motion.div className="bg-custom-turquoise lg:bg-opacity-0 lg:basis-1/3" {...fadeAnimation} id="who_we_are">
+    <motion.div className="bg-custom-turquoise lg:bg-opacity-0 lg:basis-[36%]" {...fadeAnimation} id="who_we_are">
       <div className={`${styles.container} lg:py-0`}>
-        <h1 className={`${styles.header1} text-white text-center`}>{whoWeAre?.title}</h1>
-        <p className={`${styles.body1} text-justify mt-3 lg:text-left`}>{whoWeAre?.value}</p>
+        <h1 className={`${styles.header1} text-white text-center mb-3`}>{whoWeAre?.title}</h1>
+        <ReadMore
+          initialText={initialText}
+          expandedText={expandedText}
+          className={`${styles.body1}  lg:pt-0 lg:mt-0 text-justify`}
+        />
       </div>
     </motion.div>
   );
