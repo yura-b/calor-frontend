eeef82ed3f16eb4a2a7b05b2c817c66afcb63f18
Yurii Koshyk <y.koshyk12@gmail.com>
2023-08-31 04:04:49 +0300

    fix code

--- src/api/payment.ts
+++ src/api/payment.ts
@@ -1,21 +1,23 @@
-import {backendUrl} from '@/api/languages.ts';
 import axios from 'axios';
 
+const BASE_URL = import.meta.env.VITE_BASE_URL;
+
 export const createPayPalOrder = (ordersId: string[]) => {
-    return axios.post(`${backendUrl}/paypal/create-paypal-order`, {
+    return axios.post(`${BASE_URL}/paypal/create-paypal-order`, {
         order_id: ordersId
     })
 }
 
 export const capturePayPalOrder = (data) => {
-    return axios.post(`${backendUrl}/paypal/capture-paypal-order`, {
+    return axios.post(`${BASE_URL}/paypal/capture-paypal-order`, {
         orderID: data.orderID
     })
 }
 
 export const deletePayPalID = (data) => {
-    return axios.delete(`${backendUrl}/paypal/${data.orderID}`)
+    return axios.delete(`${BASE_URL}/paypal/${data.orderID}`)
 }
+
 export const stripePayment = (order_ids: string[]) => {
-    return  axios.post(`${backendUrl}/stripe`, {order_ids})
+    return  axios.post(`${BASE_URL}/stripe`, {order_ids})
 }
