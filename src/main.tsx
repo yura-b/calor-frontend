d38562b9286c4cd624bd610e301e1d92433af9b2
Yurii Koshyk <y.koshyk12@gmail.com>
2023-09-01 09:38:57 +0300

    implement gtm manager with no events

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,15 @@ import { Provider } from 'react-redux';
 import { store } from '@/store/store.ts';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { GoogleOAuthProvider } from '@react-oauth/google';
+import TagManager from 'react-gtm-module';
+
+const gtmId = import.meta.env.VITE_GTM_ID;
+
+const tagManagerArgs = {
+  gtmId
+}
+
+TagManager.initialize(tagManagerArgs)
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
