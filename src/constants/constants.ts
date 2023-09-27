dd077ee42fc9a9e686a6cb78f9a070b8c72ebccc
Yurii Koshyk <y.koshyk12@gmail.com>
2023-09-27 15:40:28 +0300

    changing ids for dygers

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,4 +1,4 @@
 export default {
-  DAYGER_STRIPE_ID: 'price_1Nsl9JEtkmCPn31Qe5ac8UXG',
-  DAYGER_WINTER_STRIPE_ID: 'price_1Nsl9NEtkmCPn31QUdW71nta',
+  DAYGER_ID: '64d640e7e23b73b29fbddfa4',
+  DAYGER_WINTER_ID: '64f78b8d51fc954e8444bfc7',
 };
