730d3a35a480e31c93b8476e3888ec9965cf6ba7
bhd71 <m.bronytskyy@gmail.com>
2023-10-24 18:29:30 +0300

    add coupons on order page

---
+++ src/pages/admin/promocodes/components/CouponComponent.tsx
@@ -0,0 +1,17 @@
+import React, {FC} from 'react';
+import {Coupon} from '@/constants/interfaces/coupon.ts';
+import percentOrAmountCoupon from '@/helpers/functions/percentOrAmountCoupon.ts';
+import IsCouponActive from '@pages/admin/promocodes/components/IsCouponActive.tsx';
+
+const CouponComponent:FC<{coupon: Coupon}> = ({coupon}) => {
+    return (
+        <div className={'flex flex-row gap-5'}>
+            <h1 className={'font-bold'}>Coupon:</h1>
+            <p>{coupon.uuid}</p>
+            <p>{percentOrAmountCoupon(coupon.amount_off, coupon.percent_off)}</p>
+            <p><IsCouponActive isUsed={coupon.isUsed}/></p>
+        </div>
+    );
+};
+
+export default CouponComponent;
