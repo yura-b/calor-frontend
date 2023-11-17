5bfb253c31fcc6f7cc4393aa6d22488a54312d9d
Yurii Koshyk <y.koshyk12@gmail.com>
2023-11-17 11:57:36 +0200

    adding date / page title

--- src/pages/EventPage/components/Aside.tsx
+++ src/pages/EventPage/components/Aside.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import styles from '@styles/Styles.module.scss';
+import Divider from "@mui/material/Divider";
+import moment from 'moment';
 
 const Aside = ({ events }) => {
   return (
@@ -9,12 +11,18 @@ const Aside = ({ events }) => {
         <h2 className={`${styles.header1} ${styles.container} text-start p-0`}>More events</h2>
         {
           events.map((event) => (
-            <div className="flex flex-col items-start border-b border-gray">
-              <span >{event.title}</span>
-              <Link to={`/manager/event/${event._id}`} className="flex flex-col items-start justify-start">
-                <span className="text-gray font-semibold text-base underline cursor-pointer text-center py-1 font-boldmd:text-lg">Read More</span>
-              </Link>
-            </div>
+            <>
+              <div className="flex flex-col items-start">
+                <span>{event.title}</span>
+                <div className="flex flex-row justify-between w-full">
+                  <Link to={`/manager/event/${event._id}`} className="flex flex-col items-start justify-start">
+                    <span className="text-gray font-semibold text-base underline cursor-pointer text-center py-1 font-boldmd:text-lg">Read More</span>
+                  </Link>
+                  <span className="text-thinGray flex items-center italic">{moment(event.date).format('MMMM Do YYYY')}</span>
+                </div>
+                </div>
+              <Divider variant="fullWidth" />
+            </>
           ))
         }
       </div>
